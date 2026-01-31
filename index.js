d681f359bbcdd8c0e7e70bb86e8d35612e060097
unknown <loooool2312@gmail.com>
2026-02-01 05:04:37 +0900

    Update page files.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-text.textContent = "Hello, World!";
+/*
+const sorting = function (x, y) {
+  const xId = Number.parseInt(x.id.substr(1));
+  const yId = Number.parseInt(y.id.substr(1));
+  return xId - yId;
+};
+
+[...document.querySelectorAll("*[id^=c]")]
+  .sort(sorting)
+  .reverse()
+  .forEach(x => text.appendChild(x));
+*/
